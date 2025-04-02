a5754f35bd079002cb8040d3b98f60a78705ddc7
sartaj <hajamsaj@gmail.com>
2025-04-02 18:23:01 +0530

    added Objecttive

--- src/app/page.js
+++ src/app/page.js
@@ -1,21 +1,20 @@
-
 import Carousels from "@/components/Carousel";
 import Explore from "@/components/Explore";
 import Subfooter from "@/components/Subfooter";
 import React from "react";
+import OurObjective from "@/components/OurObjective";
 
 import { Car } from "lucide-react";
 
 export default function Home() {
   return (
     <div className="">
-      <Carousels/>
+      <Carousels />
       <div className="relative bg-white">
-        <Explore/>
-        <Subfooter/>
-
+        <Explore />
+        <Subfooter />
+        <OurObjective />
       </div>
     </div>
-
-  )
+  );
 }
---
+++ src/components/OurObjective.jsx
@@ -0,0 +1,35 @@
+import React from 'react'
+import TitleText from './TitleText'
+import { homeCards } from '@/constants/appdata'
+
+const OurObjective = () => {
+  return (
+    <div className='bg-teal-50 pt-12'>
+      <TitleText
+      title={
+        <span className="text-gray-900 font-bold">Our Objective</span>
+      }
+      />
+      <div className='mx-10 md:mx-[10%] mt-8'>
+        < div className='w-full grid grid-cols-1 md:grid-cols-3 justify-center gap-10'>
+          {homeCards.map((car, index) => (
+            <div key = {index} className='rounded shadow-lg bg-amber-50'>
+              <div className='w-full relative'>
+                <img src={car.image} alt="" className='w-full object-cover '/>
+        </div>
+        <div className='font-bold text-xl mb-2 text-cyan-400 p-2'>
+          {car.title}
+        </div>
+        <p className='text-gray-500 text--base p-2'>
+          {car.description}
+        </p>
+        </div>
+      ))}
+      
+      </div>
+    </div>
+    </div>
+  );
+};
+
+export default OurObjective
